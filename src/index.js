215b0d2db1029cfd7637d79de41013e1c4744317
Fabiola De La Torre <cubias.fabiola@gmail.com>
2023-07-09 22:13:32 -0500

    lab finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newTask = document.getElementById('new-task-description').value;
+    handleTask(newTask);
+    form.reset();
+  
+  });
+});    
+
+function handleTask(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deletion)
+  btn.textContent = 'x';
+  p.textContent = `${todo}`;
+  p.appendChild(btn);
+  console.log(p)
+
+  document.querySelector('#tasks').appendChild(p);
+};
+function deletion(e){
+  e.target.parentNode.remove()
+};
+
+
+
+
